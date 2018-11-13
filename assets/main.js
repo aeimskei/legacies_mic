7e1890f27fe42bc183625e1a9b4ff09886db1fdb
Amy Boudsady <aeimskei@gmail.com>
2018-11-12 19:55:50 -0800

    Initial commit: web-responsive pages, img background, navbar

---
+++ assets/main.js
@@ -0,0 +1,5 @@
+// ====================================================
+// .sidenav for Navbar Small Device
+// ====================================================
+const sideNav = document.querySelector('.sidenav');
+M.Sidenav.init(sideNav, {});
