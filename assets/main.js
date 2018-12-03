65a9c6fe886241a90c3bd56b26a4c5bdcce33da9
Amy Boudsady <aeimskei@gmail.com>
2018-12-02 19:17:09 -0800

    Removed all old files, replace with responsive version, Home layout done

--- assets/main.js
+++
@@ -1,5 +0,0 @@
-// ====================================================
-// .sidenav for Navbar Small Device
-// ====================================================
-const sideNav = document.querySelector('.sidenav');
-M.Sidenav.init(sideNav, {});
